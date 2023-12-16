ec6ad0cb4f74fd52a72938a1e12d51eb6ba373ee
miecocle <138187716+miecocle@users.noreply.github.com>
2023-12-16 12:13:57 +0700

    Add files via upload

---
+++ hethongquanli.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int admin
+{
+
+}
+int main()
+{
+    string ten;
+    cin>>ten;
+    if(ten)
+    {
+        cout<<"chao mung";
+    }
+    return 0;
+}
